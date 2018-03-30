f790c97ec221b0961e08012e4185ddda4c302327
chase-g <chasegorland@gmail.com>
2018-03-30 18:26:25 -0400

    Update Quiz.scala

--- Quiz.scala
+++ Quiz.scala
@@ -1,4 +1,7 @@
-//reusable quiz template
+object Quiz {
+  def main(args: Array[String]): Unit = {
+    
+    //reusable quiz template
     case class Quiz(questions: List[String], answers: List[List[String]], gradeSheet: Map[String, List[Int]]) { 
       import scala.util.Random.shuffle
       
@@ -60,3 +63,60 @@
     }
     
   }
+    
+    case class QandAbuilder() {
+    val q1 = "What programming domain would you like your language to excel in?"
+    val a1 = List("Web development", "Jack of all trades", "High performance, such as games or operating systems", "App development", "Not sure")
+
+    val q2 = "What kind of origin would you like your language to have?"
+    val a2 = List("Classic, with a large community", "Relatively new, and built with improvements in mind", "Created and promoted by a prestigious company.", "Not sure")
+
+    val q3 = "What kind of learning experience would you prefer?"
+    val a3 = List("Challenging, but you will learn new paradigms or algorithms along the way", "Easy to get productive quickly", "Somewhere in between", "Not sure")
+
+    val q4 = "What would your langauge's defining personality characteristic be?"
+    val a4 = List("Easy-going", "Intellectual", "Professional", "Quirky", "Not sure")
+    
+    val q5 = "What should be your language's greatest strength?"
+    val a5 = List("Logically consistent", "Concise", "Usable anywhere", "Fast", "Useful", "Not sure")
+    
+    val q6 = "What weakness could you most accept in a language?"
+    val a6 = List("Difficult to learn", "Error prone for large programs", "Verbose--lots of typing to do anything", "Inconsistent--has traps for the unwary", "Limited usage beyond a particular domain", "Lacks some features of more modern languages", "Not sure")
+    
+    val q7 = "What interests you most?"
+    val a7 = List("Data science", "Functional programming", "Object-oriented programming", "Front-end web development", "Back-end web development", "Apple ecosystem", "Microsoft ecosystem", "Systems programming", "Games and finance programming", "Not sure")  
+
+    val q8 = "Which Star Wars character do you most resemble?"
+    val a8 = List(
+        "Boba Fett: Has a large arsenal of weapons for a variety of tasks; dominant in a particular field.", 
+        "Yoda: Has been around for hundreds of years; teacher to those that follow; focused on first principles and in touch with the force.",
+        "Darth Vader: Powerful and conquest-driven; will occasionally betray his master.",
+        "Han Solo: Clever, well-rounded, and fast; responds to financial incentives.",
+        "Darth Maul: At once flashy but a man of few words.",
+        "C-3P0: Verbose but can be understood by all; a survivor that has evolved through many iterations.",
+        "R2-D2: Difficult to understand but still waters run deep; compact but useful.",
+        "Chewbacca: Terse; excels with improvised solutions to technical problems.",
+        "Jar Jar Binks: Occassionally does something useful by complete accident; repetitive."
+        )
+    val questions: List[String] = List(q1, q2, q3, q4, q5, q6, q7, q8)
+    val answers: List[List[String]] = List(a1, a2, a3, a4, a5, a6, a7, a8)
+    val languages = Map(
+      "Java" -> List(2, 1, 3, 3, 3, 3, 3, 6),
+      "Python" -> List(2, 1, 2, 1, 1, 2, 1, 8),
+      "C" -> List(3, 1, 3, 2, 4, 6, 8, 2),
+      "C++" -> List(3, 1, 1, 2, 4, 1, 9, 3),
+      "Javascript" -> List(1, 1, 2, 1, 4, 4, 1),
+      "PHP" -> List(1, 1, 2, 4, 5, 5, 5, 9),
+      "Scala" -> List(2, 2, 1, 2, 1, 1, 2, 1),
+      "C#" -> List(2, 3, 3, 3, 3, 1, 5, 7, 4),
+      "Ruby" -> List(1, 2, 2, 1, 2, 5, 5, 7),
+      "Go" -> List(2, 3, 3, 2, 1, 1, 8, 4),
+      "Swift" -> List(4, 3, 3, 3, 5, 5, 6, 4),
+      "Haskell" -> List(2, 1, 1, 2, 1, 5, 2, 9)
+      )
+    val use = Quiz(questions, answers, languages)
+    }
+    val start = QandAbuilder()
+    start.use.quiz()
+  }
+}
